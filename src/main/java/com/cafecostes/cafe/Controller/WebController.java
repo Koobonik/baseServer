651c376b40f25fd1c9260de22f7cfd2f05ebc73b
Koobonik <kbi960130@naver.com>
2019-10-24 21:46:43 +0900

    서버 베이스 작업

---
+++ src/main/java/com/cafecostes/cafe/Controller/WebController.java
@@ -0,0 +1,34 @@
+package com.cafecostes.cafe.Controller;
+
+import com.cafecostes.cafe.repository.*;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Log
+@Controller
+public class WebController {
+
+
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @RequestMapping(value="/", method = {RequestMethod.GET, RequestMethod.POST})
+    public String main() {
+        log.info("index진입");
+        return "index.html";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting.html";
+    }
+
+
+
+}
